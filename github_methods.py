1e24a7b9b6b25340ff8f19e4c7e39bfd331e13ef
erik-whiting <erik@erikwhiting.com>
2025-01-16 19:42:25 -0600

    GH methods plus issues checked file

---
+++ github_methods.py
@@ -0,0 +1,48 @@
+from github import Github, Auth
+
+
+REPO_NAME = "biopython"
+REPO_PATH = f"erik-whiting/{REPO_NAME}"
+
+
+def authenticate():
+    with open("../issue_reader_token") as fh:
+        access_token = fh.read()
+
+    auth = Auth.Token(access_token)
+    g = Github(auth=auth)
+
+    return g
+
+
+def get_repo():
+    auth = authenticate()
+    repo = auth.get_repo(REPO_PATH)
+    return repo
+
+
+def get_issues():
+    repo = get_repo()
+    repo.issue
+    issues = repo.get_issues()
+    return issues
+
+
+def issue_key(issue):
+    return issue.title.replace(" ", "-") + f"-{issue.number}"
+
+
+def mark_issue_checked(issue):
+    with open("issues_checked.txt", "a") as fh:
+        fh.write(f"{issue_key(issue)}\n")
+
+
+def new_issues():
+    with open("issues_checked.txt") as fh:
+        old_issues = [i.strip() for i in fh.readlines()]
+    new_issues = []
+    for i in get_issues():
+        key = issue_key(i)
+        if key not in old_issues:
+            new_issues.append(i)
+    return new_issues
